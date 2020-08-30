eded56fea4364a9b308420c2d143b5b176c0b59b
fatma hassan <nihankamal2020@gmail.com>
2020-08-31 00:49:44 +0300

    used only one constructor Class to Summation 2 numbers [sum(5,7)]and print the result from main method

---
+++ src/one constructor.kt
@@ -0,0 +1,19 @@
+import java.lang.Integer.sum
+
+fun main()
+{
+    Sum(10,2)
+}
+
+
+
+
+class Sum(number1:Int , number2:Int){
+
+    init {
+        var Summation=number1+number2;
+        println("Add the two numbers = $Summation ")
+    }
+
+
+}
